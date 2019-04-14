12f8350373febf162704a7276d91f84eef38e2f5
AdlerPagliarini <adlerpagliarini@gmail.com>
2019-04-14 09:51:25 -0300

    'TornandoPaginasAcessiveis'

--- js/dialog.js
+++ js/dialog.js
@@ -3,17 +3,24 @@ var btnAbreDialog = document.querySelector('#abreDialog');
 var dialog = document.querySelector('.dialogNewsletter');
 var dialogBody = document.querySelector('.dialogNewsletter-body');
 var dialogOverlay = document.querySelector('.dialogNewsletter-overlay');
+var dialogInput = dialog.querySelector('input');
+var ignore = document.querySelectorAll('header, main, footer');
 
 
 // Quando abrir a dialog...
 btnAbreDialog.addEventListener('click', function() {
+  ignore.forEach(e => e.setAttribute('inert', 'true'));
   dialog.classList.add('dialogNewsletter--aberto');
+  dialogInput.focus();
 });
 
 function fechandoDialog() {
+  ignore.forEach(e => e.removeAttribute('inert'));
   document.activeElement.blur();
   dialog.classList.remove('dialogNewsletter--aberto');     
+  btnAbreDialog.focus();
 }
 
 // Listeners
 document.querySelector('.dialogNewsletter-fechar').addEventListener('click', fechandoDialog);
+btnAbreDialog.style.display = 'block';
